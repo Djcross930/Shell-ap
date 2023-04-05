70da6b4211a20aa4a99b0bcc11173bd902074b0e
djcross930 <dancross96@gmail.com>
2023-04-05 10:25:06 -0500

    added comments in OnThisDayCommand.java for some context.

--- src/main/java/com/example/onthisday/OnThisDayCommand.java
+++ src/main/java/com/example/onthisday/OnThisDayCommand.java
@@ -6,11 +6,11 @@ import org.springframework.web.client.RestTemplate;
 
 @ShellComponent
 public class OnThisDayCommand {
-    @ShellMethod("Prints events from https://byabbe.se/on-this-day/{month}/{day}/events.json")
+    @ShellMethod("Prints events from https://byabbe.se/on-this-day/{month}/{day}/events.json") //Run events in shell along with a month and date. for example, type in "events 3 1" to receive the full JSON response of events on March 1st
     public String events(int month, int day) {
         String url = String.format("https://byabbe.se/on-this-day/%d/%d/events.json", month, day);
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject(url, String.class);
-        return response;
+        return response; //need to further parse the response to include only title and description, right now it has full JSON response only
     }
 }
