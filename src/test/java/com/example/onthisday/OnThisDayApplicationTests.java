8e4e5e5602d0bcb3cec35b6c6ec15a076480e16e
djcross930 <dancross96@gmail.com>
2023-04-10 13:10:47 -0500

    Testing stuff(broken)

--- src/test/java/com/example/onthisday/OnThisDayApplicationTests.java
+++ src/test/java/com/example/onthisday/OnThisDayApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.onthisday;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-class OnThisDayApplicationTests {
+class OnThisDayCommandTest {
+
+    @Autowired
+    private OnThisDayCommand onThisDayCommand;
 
     @Test
-    void contextLoads() {
+    @Timeout(10) // Set a timeout of 10 seconds
+    void eventsReturnsData() {
+        String result = onThisDayCommand.events(3, 1);
+        assertNotNull(result);
     }
-
 }
